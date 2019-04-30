acbb1458b53fe13d80bbcbf90d431ab041de198a
Frederick Kautz <frederick@doc.ai>
2019-04-30 17:38:41 -0600

    Remove generate-proto script
    Signed-off-by: Frederick Kautz <frederick@doc.ai>

--- api/generate-proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh +e
-GRPC_GATEWAY_PROTO_DIR="$(go list -m -f "{{.Dir}}" github.com/grpc-ecosystem/grpc-gateway)/third_party/googleapis"
-GOPATH="$(go env GOPATH)"
-
-protoc -I . repository.proto --go_out=plugins=grpc:. --proto_path=$GOPATH/src --proto_path=$GOPATH/pkg/mod --proto_path=$GRPC_GATEWAY_PROTO_DIR
-protoc -I . repository.proto --grpc-gateway_out=logtostderr=true:. --proto_path=$GOPATH/src --proto_path=$GOPATH/pkg/mod --proto_path=$GRPC_GATEWAY_PROTO_DIR
-protoc -I . repository.proto --swagger_out=logtostderr=true:. --proto_path=$GOPATH/src --proto_path=$GOPATH/pkg/mod --proto_path=$GRPC_GATEWAY_PROTO_DIR
