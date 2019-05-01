7d3643a4a7fcc50e5b8c450e62b0a438e6beaf42
Neeraj Kashyap <neeraj@doc.ai>
2019-05-01 05:48:02 -0700

    Added -backend flag to repository command
    Also modified:
    1. Dockerfile.repository to accept repository flags from docker run via
    CMD
    2. Makefile to accept a RUN_ARGS variable for `make run`.

--- cmd/repository/main.go
+++ cmd/repository/main.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"github.com/doc-ai/tensorio-models/api"
 	"github.com/doc-ai/tensorio-models/server"
+	"github.com/doc-ai/tensorio-models/storage"
 	"github.com/doc-ai/tensorio-models/storage/memory"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"net"
 	"net/http"
+	"strings"
 	"time"
 )
 
-func startGrpcServer() {
+func startGrpcServer(apiServer api.RepositoryServer) {
 	serverAddress := ":8080"
 	log.Println("Starting grpc on:", serverAddress)
-	var srv api.RepositoryServer
-	srv = server.NewServer(memory.NewMemoryRepositoryStorage())
 
 	grpcServer := grpc.NewServer()
 	lis, err := net.Listen("tcp", serverAddress)
-	api.RegisterRepositoryServer(grpcServer, srv)
+	api.RegisterRepositoryServer(grpcServer, apiServer)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -55,7 +57,35 @@ func startProxyServer() {
 }
 
 func main() {
-	go startGrpcServer()
+	/* BEGIN command line argument parsing */
+	// Backend specification
+	Backends := map[string]func() storage.RepositoryStorage{
+		"memory": memory.NewMemoryRepositoryStorage,
+	}
+	BackendKeys := make([]string, len(Backends))
+	i := 0
+	for k := range Backends {
+		BackendKeys[i] = k
+		i++
+	}
+	BackendChoices := strings.Join(BackendKeys, ",")
+
+	var backendArg string
+	backendUsage := fmt.Sprintf("Specifies the repository storage backend to be used; choices: %s", BackendChoices)
+	flag.StringVar(&backendArg, "backend", "", backendUsage)
+
+	flag.Parse()
+
+	backend, exists := Backends[backendArg]
+	if !exists {
+		log.Fatalf("Unknown backend: %s. Choices are: %s", backendArg, BackendChoices)
+	}
+	/* END command line argument parsing */
+
+	repositoryBackend := backend()
+	apiServer := server.NewServer(repositoryBackend)
+
+	go startGrpcServer(apiServer)
 	go startProxyServer()
 	time.Sleep(1 * time.Hour)
 }
