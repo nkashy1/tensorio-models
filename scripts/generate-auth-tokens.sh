8a49196c072d73fe6f67d6853d5978167704918c
Neeraj Kashyap <nkashy1@gmail.com>
2019-06-04 18:36:51 -0700

    Added auth token generator script (#145)

---
+++ scripts/generate-auth-tokens.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+echo "FleaClient     $(uuidgen)"
+echo "FleaClient     $(uuidgen)"
+echo "FleaTaskGen    $(uuidgen)"
+echo "FleaAdmin      $(uuidgen)"
+echo "ModelsAdmin    $(uuidgen)"
+echo "ModelsReader   $(uuidgen)"
+echo "ModelsReader   $(uuidgen)"
+echo "ModelsWriter   $(uuidgen)"
