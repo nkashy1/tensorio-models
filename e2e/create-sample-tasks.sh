2e24710999806cbd1ad3fe81b741c2135c41f7b0
Neeraj Kashyap <nkashy1@gmail.com>
2019-06-04 18:37:13 -0700

    Parametrized tokens in e2e scripts (#146)
    setup.sh for respository, create-sample-tasks.sh for flea.

--- e2e/create-sample-tasks.sh
+++ e2e/create-sample-tasks.sh
@@ -9,7 +9,10 @@
 # One can also provide a server:port as argument.
 
 API_URL=${1:-localhost:8083}
-FLEA_URL=http://$API_URL/v1/flea
+FLEA_URL=$API_URL/v1/flea
+FLEA_ADMIN_TOKEN=${FLEA_ADMIN_TOKEN:-AdminToken}
+FLEA_TASKGEN_TOKEN=${FLEA_TASKGEN_TOKEN:-TaskGenToken}
+FLEA_CLIENT_TOKEN=${FLEA_CLIENT_TOKEN:-ClientToken}
 
 TIMESTAMP=$(date -u +%s)
 MODEL="TestModel-$TIMESTAMP"
@@ -26,21 +29,21 @@ curl -s -X GET $FLEA_URL/config
 
 echo ""
 echo "Create Task: 101"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model17\", \"hyperparametersId\": \"hpset5\", \"checkpointId\": \"ckpt-7\", \"taskId\": \"task101\", \"deadline\": \"2019-12-31T23:59:59.001Z\", \"active\": false, \"link\": \"http://goo.gl/T1.zip\"}" \
     $FLEA_URL/create_task
 
 echo ""
 echo "Create Task: 102"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model3\", \"hyperparametersId\": \"hpset3\", \"checkpointId\": \"ckpt-3\", \"taskId\": \"task102\", \"deadline\": \"2019-07-31T23:59:59.002Z\", \"active\": true, \"link\": \"http://goo.gl/T2.zip\"}" \
     $FLEA_URL/create_task
 
 echo ""
 echo "Create Task: aaa"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model5\", \"hyperparametersId\": \"hpset13\", \"checkpointId\": \"ckpt-2\", \"taskId\": \"aaa\", \"deadline\": \"2019-12-14T23:59:59.003Z\", \"active\": true, \"link\": \"http://goo.gl/T3.zip\"}" \
     $FLEA_URL/create_task
@@ -48,92 +51,92 @@ curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
 
 echo ""
 echo "List all tasks:"
-curl -H "Authorization: Bearer ClientToken" "$FLEA_URL/tasks?includeInactive=true"
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" "$FLEA_URL/tasks?includeInactive=true"
 
 echo ""
 echo "List all ACTIVE tasks:"
-curl -H "Authorization: Bearer ClientToken" $FLEA_URL/tasks
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/tasks
 
 echo ""
 echo "Create Task: x3"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model5\", \"hyperparametersId\": \"hpset3\", \"checkpointId\": \"ckpt-5\", \"taskId\": \"x3\", \"deadline\": \"2023-12-31T23:59:00.004Z\", \"active\": true, \"link\": \"http://goo.gl/Tx3.zip\"}" \
     $FLEA_URL/create_task
 
 echo ""
 echo "Create Task: y5"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model15\", \"hyperparametersId\": \"hpset1\", \"checkpointId\": \"ckpt-42\", \"taskId\": \"y5\", \"deadline\": \"2019-12-21T23:59:59.005Z\", \"active\": true, \"link\": \"http://goo.gl/Tx5.zip\"}" \
     $FLEA_URL/create_task
 
 echo ""
 echo "Create Task: b7"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"modelId\": \"Model-bozo5\", \"hyperparametersId\": \"hpset1\", \"checkpointId\": \"ckpt-52\", \"taskId\": \"b7\", \"deadline\": \"2029-12-31T23:59:59.006Z\", \"active\": true, \"link\": \"http://goo.gl/Tzxe3.zip\"}" \
     $FLEA_URL/create_task
 
 echo ""
 echo "List first 3 tasks:"
-curl -H "Authorization: Bearer ClientToken" "$FLEA_URL/tasks?inludeInactive=true&maxItems=3"
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" "$FLEA_URL/tasks?inludeInactive=true&maxItems=3"
 
 echo ""
 echo "List tasks starting from b7:"
-curl -H "Authorization: Bearer ClientToken" "$FLEA_URL/tasks?includeInactive=true&startTaskId=b7"
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" "$FLEA_URL/tasks?includeInactive=true&startTaskId=b7"
 
 echo ""
 echo "List 2 tasks starting from task101:"
-curl -H "Authorization: Bearer ClientToken" "$FLEA_URL/tasks?includeInactive=true&startTaskId=task101&maxItems=2"
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" "$FLEA_URL/tasks?includeInactive=true&startTaskId=task101&maxItems=2"
 
 echo ""
 echo "Get details for task x3:"
-curl -H "Authorization: Bearer ClientToken" $FLEA_URL/tasks/x3
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/tasks/x3
 
 echo ""
 echo "Modify task x3:"
-curl -s -X POST -H "Authorization: Bearer TaskGenToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_TASKGEN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"deadline\": \"2020-02-12T23:00:00.888888Z\", \"active\": false}" \
     $FLEA_URL/modify_task/x3
 
 echo ""
 echo "Get details for task x3:"
-curl -s  -H "Authorization: Bearer ClientToken" $FLEA_URL/tasks/x3
+curl -s  -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/tasks/x3
 
 echo ""
 echo "Reload credentials:"
-curl -s  -X POST -H "Authorization: Bearer AdminToken" \
+curl -s  -X POST -H "Authorization: Bearer $FLEA_ADMIN_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"type\": 1}" \
     $FLEA_URL/admin
 
 echo 
 echo "Get details for task x3:"
-curl -H "Authorization: Bearer ClientToken" $FLEA_URL/tasks/x3
+curl -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/tasks/x3
 
 echo ""
 echo "Start task b7:"
-curl -s  -H "Authorization: Bearer ClientToken" $FLEA_URL/start_task/b7
+curl -s  -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/start_task/b7
 
 echo ""
 echo "Start task b7 (emulate another client):"
-curl -s  -H "Authorization: Bearer ClientToken" $FLEA_URL/start_task/b7
+curl -s  -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/start_task/b7
 
 echo ""
 echo "Unauthorized 1:"
-curl -s  -H "Authorization: ClientToken" $FLEA_URL/start_task/b7
+curl -s  -H "Authorization: $FLEA_CLIENT_TOKEN" $FLEA_URL/start_task/b7
 
 echo ""
 echo "Unauthorized 2:"
 curl -s  $FLEA_URL/start_task/b7
 
-JOBID=$(curl -s  -H "Authorization: Bearer ClientToken" $FLEA_URL/start_task/x3 | jq .jobId | tr -d \")
+JOBID=$(curl -s  -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" $FLEA_URL/start_task/x3 | jq .jobId | tr -d \")
 
 echo
 echo "Add Task Error Report"
-curl -s -X POST -H "Authorization: Bearer ClientToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" \
      -H "Content-Type: application/json" \
      -d "{\"errorMessage\": \"Save this error!\"}" \
      $FLEA_URL/job_error/x3/$JOBID
@@ -147,7 +150,7 @@ curl -s -X POST \
 
 echo
 echo "Valid log request"
-curl -s -X POST -H "Authorization: Bearer ClientToken" \
+curl -s -X POST -H "Authorization: Bearer $FLEA_CLIENT_TOKEN" \
      -H "Content-Type: application/json" \
      -d "{\"message\": \"Only besties make it into the log.\"}" \
      $FLEA_URL/log/BestClient
--- e2e/setup.sh
+++ e2e/setup.sh
@@ -15,6 +15,7 @@
 # $ API_URL=<remote URL> FILE_SERVER_URL=localhost:8000 ./setup.sh
 
 API_URL=${API_URL:-localhost:8081}
+API_TOKEN=${API_TOKEN:-WriterToken}
 FILE_SERVER_URL=${FILE_SERVER_URL:-http://example.com}
 
 TIMESTAMP=$(date -u +%s)
@@ -26,7 +27,7 @@ echo "Setting up API instance at: $API_URL"
 
 echo "Creating model: $MODEL ..."
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"model\": {\"modelId\": \"$MODEL\", \"details\": \"This model is a test\", \"canonicalHyperparameters\": \"hyperparameters-2\"}}" \
     $API_URL/v1/repository/models
@@ -35,7 +36,7 @@ echo
 
 echo "Creating hyperparameters-1..."
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{"hyperparametersId": "hyperparameters-1", "hyperparameters": {"lol": "rofl"}}' \
     $API_URL/v1/repository/models/$MODEL/hyperparameters
@@ -44,7 +45,7 @@ echo
 
 echo "Creating checkpoint-1 for hyperparameters-1..."
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"checkpointId\": \"checkpoint-1\", \"createdAt\": \"1557790163\", \"info\": {\"accuracy\": \"0.934\"}, \"link\": \"$FILE_SERVER_URL/h1c1.tiobundle.zip\"}" \
     $API_URL/v1/repository/models/$MODEL/hyperparameters/hyperparameters-1/checkpoints
@@ -53,7 +54,7 @@ echo
 
 echo "Setting checkpoint-1 as canonicalCheckpoint for hyperparameters-1 and hyperparameters-2 as its upgrade..."
 curl -X PUT \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{"canonicalCheckpoint": "checkpoint-1", "upgradeTo": "hyperparameters-2"}' \
     $API_URL/v1/repository/models/$MODEL/hyperparameters/hyperparameters-1
@@ -62,7 +63,7 @@ echo
 
 echo "Creating hyperparameters-2"
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{"hyperparametersId": "hyperparameters-2", "hyperparameters": {"lol": "wtf"}}' \
     $API_URL/v1/repository/models/$MODEL/hyperparameters
@@ -71,7 +72,7 @@ echo
 
 echo "Creating checkpoint-1 for hyperparameters-2..."
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"checkpointId\": \"checkpoint-1\", \"createdAt\": \"1557790252\", \"info\": {\"accuracy\": \"0.921\"}, \"link\": \"$FILE_SERVER_URL/h2c1.tiobundle.zip\"}" \
     $API_URL/v1/repository/models/$MODEL/hyperparameters/hyperparameters-2/checkpoints
@@ -80,7 +81,7 @@ echo
 
 echo "Creating checkpoint-2 for hyperparameters-2..."
 curl -X POST \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d "{\"checkpointId\": \"checkpoint-2\", \"createdAt\": \"1557790268\", \"info\": {\"accuracy\": \"0.959\"}, \"link\": \"$FILE_SERVER_URL/h2c2.tiobundle.zip\"}" \
     $API_URL/v1/repository/models/$MODEL/hyperparameters/hyperparameters-2/checkpoints
@@ -89,7 +90,7 @@ echo
 
 echo "Setting checkpoint-2 as canonicalCheckpoint for hyperparameters-2..."
 curl -X PUT \
-    -H "Authorization: Bearer WriterToken" \
+    -H "Authorization: Bearer $API_TOKEN" \
     -H "Content-Type: application/json" \
     -d '{"canonicalCheckpoint": "checkpoint-2"}' \
     $API_URL/v1/repository/models/$MODEL/hyperparameters/hyperparameters-2
